25a269b693512c6eb9b4af61c633225dc903bb1b
markawaken <mark.evans@awakensoftware.com>
2019-08-08 23:25:44 +0100

    Refactor

--- InterviewTask/Services/HelperApiService.cs
+++ InterviewTask/Services/HelperApiService.cs
@@ -7,9 +7,10 @@ namespace InterviewTask.Services
 {
     public class HelperApiService:BaseApi, IHelperApiService
     {
-        public HelperApiService(string baseUrl) : base(baseUrl)
+        
+        public HelperApiService(string baseUrl, ILoggingService LoggingService) : base(baseUrl,LoggingService)
         {
-            
+           
         }
         public dynamic GetWeatherByCity(string cityName,string CountryCode=null)
         {
